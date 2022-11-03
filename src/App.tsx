2f4a4429dbb86627a0e847679c163cbed83d16b8
arsen056 <vurdihanov@gmail.com>
2022-11-03 22:02:48 +0300

    feat: add gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Button from "./components/Button";
-import button from "./components/Button";
 
 function App() {
 
